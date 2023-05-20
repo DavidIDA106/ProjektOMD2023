345d923b9b13b616cb47bdefca0bf4a8eab27a94
Psi <psisisu@gmail.com>
2023-05-20 15:46:23 +0200

    Is this a hack?

--- src/computer/Program.java
+++ src/computer/Program.java
@@ -1,10 +1,40 @@
 package computer;
 
+import instruction.Instruction;
+
+import java.util.List;
+
 public class Program {
 
     private ProgramCounter pc;
 
+    private List<Instruction> instList;
+
     public Program () {
         pc = new ProgramCounter();
+    }
+
+    public boolean add (Instruction i) {
+        instList.add(i);
+        return true;
+    }
+
+    public void reset () {
+        pc.setIndex(0);
+    }
+
+    public void run () {
+
+        int ret = 0;
+
+        while (ret >= 0) {
+
+            ret = instList.get(pc.getIndex()).exec();
+
+            if (ret > 0) {
+                pc.setIndex(ret);
+            }
+            else pc.inc();
+        }
     }
 }
